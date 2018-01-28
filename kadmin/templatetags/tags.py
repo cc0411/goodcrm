7a2e3cf5ba73b5daf646a42bccfd40f11ec906ca
小磊 <admin@jcwit.com>
2018-01-28 17:31:55 +0800

    Update tags.py

--- kadmin/templatetags/tags.py
+++ kadmin/templatetags/tags.py
@@ -22,14 +22,18 @@ def render_page_ele(loop_counter,query_sets):
     return ''
 
 @register.simple_tag
-def build_tables_row(obj,admin_class):
+def build_table_row(obj,admin_class):
     row_ele = ""
     for column in admin_class.list_display:
         field_obj = obj._meta.get_field(column)
-        if field_obj.choices:
-            column_data = getattr(obj,"get_%s_display" %column)()
-        column_data = getattr(obj,column)
-        if type(column_data).__name__ =='datetime' :
+        if field_obj.choices:#choices type
+            column_data = getattr(obj,"get_%s_display" % column)()
+        else:
+            column_data = getattr(obj,column)
+
+        if type(column_data).__name__ == 'datetime':
             column_data = column_data.strftime("%Y-%m-%d")
-        row_ele += "<td>%s</td>" %column_data
-    return  mark_safe(row_ele)
+
+        row_ele += "<td>%s</td>" % column_data
+
+    return mark_safe(row_ele)
